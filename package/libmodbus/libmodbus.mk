670ae132b5d797296d0621d38cda899e85571ede
Arthur <arthur@nuvoton.com>
2024-04-22 20:42:43 -0400

    libmodbus 3.1.10

--- package/libmodbus/libmodbus.mk
+++ package/libmodbus/libmodbus.mk
@@ -4,8 +4,9 @@
 #
 ################################################################################
 
-LIBMODBUS_VERSION = 3.0.6
-LIBMODBUS_SITE = http://libmodbus.org/releases
+LIBMODBUS_VERSION = 3.1.10
+LIBMODBUS_SOURCE = libmodbus-$(LIBMODBUS_VERSION).tar.gz
+LIBMODBUS_SITE = https://github.com/stephane/libmodbus/releases/download/v$(LIBMODBUS_VERSION)
 LIBMODBUS_LICENSE = LGPLv2.1+
 LIBMODBUS_LICENSE_FILES = COPYING.LESSER
 LIBMODBUS_INSTALL_STAGING = YES
